ac3baded90ec28211323fa6ba283c28c380e11e7
salma <sallmayasser512@gmail.com>
2024-02-24 23:07:46 +0200

    add scan in reservation model

--- models/doctorReservationModel.js
+++ models/doctorReservationModel.js
@@ -1,27 +1,31 @@
 
 const mongoose = require('mongoose');
 ///1)create schema
-const doctorReservationsSchema = new mongoose.Schema({
+const doctorReservationsSchema = new mongoose.Schema(
+  {
     date: {
-        type: Date,
-        required: [true,"reservation date is required"]
+      type: Date,
+      required: [true, 'reservation date is required'],
     },
     uploadedTest: {
-        type: String,
-        default: null,
-    },patient
-    : {
-        type: mongoose.Schema.ObjectId,
-        ref: "Patient",
-
+      type: String,
+      default: null,
     },
-    dermatologist :{
-        type: mongoose.Schema.ObjectId,
-        ref: "Dermatologist",
-
+    patient: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Patient',
     },
-    
-}, { timestamps: true })
+    dermatologist: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Dermatologist',
+    },
+    scan: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Scans',
+    },
+  },
+  { timestamps: true },
+);
 doctorReservationsSchema.pre(/^find/, function (next){
     this.populate({
         path: 'patient',
@@ -35,6 +39,14 @@ doctorReservationsSchema.pre(/^find/, function (next){
         select: "firstName lastName ",
     })
     next()
+});
+
+doctorReservationsSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'scan',
+    select: 'diseasePhoto diseaseName ',
+  });
+  next();
 });
 ///2)create model
 module.exports = mongoose.model("DoctorReservation", doctorReservationsSchema);
