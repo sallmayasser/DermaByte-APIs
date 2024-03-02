7185fa10a40eea421282d8bafd88d11cf9dda034
salma <sallmayasser512@gmail.com>
2024-03-03 01:52:25 +0200

    modifiy route

--- Routes/reportRoute.js
+++ Routes/reportRoute.js
@@ -1,19 +1,29 @@
 const express = require('express');
-const { getReportValidator, createReportValidator ,updateReportValidator, deleteReportValidator } = require("../utils/validators/reportValidator")
+const {
+  getReportValidator,
+  createReportValidator,
+  updateReportValidator,
+  deleteReportValidator,
+} = require('../utils/validators/reportValidator');
 
-const { getReports ,createReport, getReport, updateReport, deleteReport, appendReport } = require("../controllers/reportController");
+const {
+  getReports,
+  createReport,
+  getReport,
+  updateReport,
+  deleteReport,
+  appendReport,
+} = require('../controllers/reportController');
 
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getReports)
-    .post(createReportValidator,createReport);
+router.route('/').get(getReports).post(createReportValidator, createReport);
 
-router.route('/:id')
-    //getReportValidator validation layer  rule call validator 
-    .get(getReportValidator, getReport)
-    .put(updateReportValidator, updateReport)
-    .delete (deleteReportValidator, deleteReport);
 router
-  .route('/test/:id')
-  .put(updateReportValidator, appendReport)
+  .route('/:id')
+  //getReportValidator validation layer  rule call validator
+  .get(getReportValidator, getReport)
+  .put(updateReportValidator, updateReport)
+  .delete(deleteReportValidator, deleteReport);
+router.route('/:id/test').put(updateReportValidator, appendReport);
 module.exports = router;
