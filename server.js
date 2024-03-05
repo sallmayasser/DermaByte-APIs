171fd8438f84dc774bbc0a4cc0fa1b6f5299c826
salma <sallmayasser512@gmail.com>
2024-03-05 14:51:50 +0200

    compress app

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
+const compression = require('compression');
 
 const ApiError = require('./utils/apiError');
 const globalError = require('./middleware/errorMiddleware');
@@ -29,6 +30,8 @@ const app = express();
 
 
 /////middleware
+app.use(cors());
+app.use(compression());
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'uploads')));
 
