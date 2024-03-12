45bebe149f82adc8e38a03107ead019afbc01bc9
salma <sallmayasser512@gmail.com>
2024-03-12 03:01:25 +0200

    correct error

--- controllers/scansController.js
+++ controllers/scansController.js
@@ -4,26 +4,26 @@ const { v4: uuidv4 } = require('uuid');
 
 const Scans = require('../models/scansModel');
 const factory = require('./handlersFactory');
-const{uploadSingleImage }=require('../middleware/uploadImageMiddleware');
+const { uploadSingleImage } = require('../middleware/uploadImageMiddleware');
 
 // Upload single image
-exports.uploadScanImage =uploadSingleImage('diseasePhoto');
+exports.uploadScanImage = uploadSingleImage('diseasePhoto');
 
 // Image processing
 exports.resizeScanImage = asyncHandler(async (req, res, next) => {
   const filename = `scanphoto-${uuidv4()}-${Date.now()}.jpeg`;
-if(req.file){
-  await sharp(req.file.buffer)
-    .resize(600, 600)
-    .toFormat('jpeg')
-    .jpeg({ quality: 95 })
-    .toFile(`uploads/scans/${filename}`);
+  if (req.file) {
+    await sharp(req.file.buffer)
+      .resize(600, 600)
+      .toFormat('jpeg')
+      .jpeg({ quality: 95 })
+      .toFile(`uploads/scans/${filename}`);
 
-  // Save image into our db 
-  
-  //  req.body.license = filename;
-   req.body.diseasePhoto = filename;
-}
+    // Save image into our db
+
+    //  req.body.license = filename;
+    req.body.diseasePhoto = filename;
+  }
   next();
 });
 exports.getScans = factory.getAll(Scans);
@@ -33,6 +33,6 @@ exports.deleteScan = factory.deleteOne(Scans);
 
 exports.setPatientIdToBody = (req, res, next) => {
   // Nested route (Create)
-  if (!req.body.patient) req.body.patient = req.params.patientId;
+  if (!req.body.patient) req.body.patient = req.params.id;
   next();
 };
