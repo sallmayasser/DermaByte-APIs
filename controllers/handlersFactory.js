378a58ab587f618088ba6ebbf5326dd4c1168221
salma <sallmayasser512@gmail.com>
2024-03-13 15:56:06 +0200

    modify error

--- controllers/handlersFactory.js
+++ controllers/handlersFactory.js
@@ -14,7 +14,7 @@ exports.deleteOne = (Model) =>
     }
 
    // Trigger "remove" event when update document
-    document.remove();
+    // document.remove();
     res.status(204).json({
       status: 'success',
       data: null,
