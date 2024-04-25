eaf041daec4e163d1433f9c77ee411223548206f
Martina <martinagaras2030@outlook.com>
2024-04-25 21:42:57 +0200

    payment

--- Routes/paymentRoute.js
+++ Routes/paymentRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { checkoutSession } = require('../controllers/paymentController');
 const authController = require('../controllers/authController');
-
+const functions = require('../controllers/doctorReservationController');
+const { createFilterObj, getLoggedUserData } = require('../controllers/handlersFactory');
 const router = express.Router();
 
 router.get('/checkout-session',
