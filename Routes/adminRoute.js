4bd3e9354bf0f26d7c7e69c8db90d543d82297aa
salma <sallmayasser512@gmail.com>
2024-05-01 22:16:07 +0300

    add decline function

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-const { Approve } = require('../controllers/adminController');
+const { Approve, Decline } = require('../controllers/adminController');
 
 const router = express.Router({ mergeParams: true });
 
 router
   .route('/Approve')
-  .put(
-    authController.protect,
-    authController.allowedTo('admin'),
-    Approve,
-  );
-  
+  .put(authController.protect, authController.allowedTo('admin'), Approve);
+router
+  .route('/Decline')
+  .delete(authController.protect, authController.allowedTo('admin'), Decline);
 module.exports = router;
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Labs = require('../models/labModel');
-const Dermatologist = require('../models/dermatologistModel')
+const Dermatologist = require('../models/dermatologistModel');
 const ApiError = require('../utils/apiError');
 
 exports.Approve = asyncHandler(async (req, res, next) => {
@@ -30,4 +30,31 @@ exports.Approve = asyncHandler(async (req, res, next) => {
       }
     })
     .catch(next);
+});
+exports.Decline = asyncHandler(async (req, res, next) => {
+  // Execute both queries concurrently using Promise.all
+  Promise.all([
+    Labs.findByIdAndDelete(req.body.id),
+    Dermatologist.findByIdAndDelete(req.body.id),
+  ])
+    .then(([foundLab, foundDermatologist]) => {
+      if (foundLab) {
+        res
+          .status(200)
+          .json({
+            message: 'you have Decline this user And deleted successfully.',
+          });
+      } else if (foundDermatologist) {
+        res.status(200).json({
+          message: 'you have Decline this user And deleted successfully.',
+        });
+      } else {
+        return Promise.reject(
+          new ApiError(`No user found for this id ${req.body.id}`, 404),
+        );
+      }
+    })
+    .catch((error) => {
+      next(new ApiError('Server failed', 500));
+    });
 });
