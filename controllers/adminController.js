be99647e149b491ff451c36403ed19a71d9f8a6c
salma <sallmayasser512@gmail.com>
2024-05-11 00:47:45 +0300

    send mail for  approved mail

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -15,12 +15,15 @@ exports.Approve = asyncHandler(async (req, res, next) => {
     ),
   ])
     .then(([foundLabs, foundDermatologist]) => {
-      // Check if user is found in Labss
+      let foundUser;
+      // Check if user is found in Labs
       if (foundLabs) {
+        foundUser = foundLabs;
         res.status(200).json({ data: foundLabs });
       }
       // Check if user is found in dermatologists
       else if (foundDermatologist) {
+        foundUser = foundDermatologist; // Corrected assignment
         res.status(200).json({ data: foundDermatologist });
       }
       // If user is not found in either model, return 404
@@ -29,9 +32,46 @@ exports.Approve = asyncHandler(async (req, res, next) => {
           new ApiError(`No user found for this id ${req.body.id}`, 404),
         );
       }
+
+      const message = `Dear ${foundUser.firstName},
+
+\nWe are pleased to inform you that your account has been approved. You are now a registered member of DermaByte. We would like to extend a warm welcome to our platform.
+
+\nYour account details:
+- Username: ${foundUser.firstName}
+- Email: ${foundUser.email}
+
+\nPlease feel free to log in to your account 
+
+\nIf you have any questions or need assistance, please don't hesitate to contact our support team at <dermabyte2024@gmail.com>.
+
+\nThank you for joining DermaByte. We look forward to serving you and providing you with a seamless experience.
+
+\nBest regards,
+The DermaByte application Team 
+
+`;
+
+      return sendEmail({
+        email: foundUser.email,
+        subject: 'Account Accepted',
+        message,
+      })
+        .then(() => {
+          // Send success response after sending email and updating user
+          res.status(200).json({
+            message:
+              'You have accepted this user successfully. An email notification has been sent.',
+          });
+        })
+        .catch((error) => {
+          // If there's an error sending email, handle it
+          return Promise.reject(new ApiError('Failed to send email', 500));
+        });
     })
-    .catch(next);
+    .catch(next); // Catch any errors during the updates or email sending
 });
+
 exports.Decline = asyncHandler(async (req, res, next) => {
   Promise.all([
     Labs.findByIdAndDelete(req.body.id),
@@ -53,20 +93,20 @@ exports.Decline = asyncHandler(async (req, res, next) => {
 
 \nI hope this message finds you well.
 
-\n\nI am writing to inform you about the status of your recent account application with DermaByte . We regret to inform you that your application has been declined due to incomplete license information provided during the registration process.
+\nI am writing to inform you about the status of your recent account application with DermaByte . We regret to inform you that your application has been declined due to incomplete license information provided during the registration process.
 
-\n\nAt DermaByte , we strive to maintain the highest standards of compliance and accuracy in our records. Unfortunately, the information provided in your license documentation did not meet our requirements for verification purposes.
+\nAt DermaByte , we strive to maintain the highest standards of compliance and accuracy in our records. Unfortunately, the information provided in your license documentation did not meet our requirements for verification purposes.
 
-\n\nWe understand the inconvenience this may cause and sincerely apologize for any frustration or disappointment this decision may have caused you. It is never our intention to inconvenience our applicants, but rather to ensure the integrity and security of our platform for all users.
+\nWe understand the inconvenience this may cause and sincerely apologize for any frustration or disappointment this decision may have caused you. It is never our intention to inconvenience our applicants, but rather to ensure the integrity and security of our platform for all users.
 
-\n\nWe value your interest in DermaByte  and encourage you to review and resubmit your application with the required documentation to complete the registration process. Should you have any questions or require further assistance, please do not hesitate to contact our support team at <dermabyte2024@gmail.com>.
+\nWe value your interest in DermaByte  and encourage you to review and resubmit your application with the required documentation to complete the registration process. Should you have any questions or require further assistance, please do not hesitate to contact our support team at <dermabyte2024@gmail.com>.
 
-\n\nOnce again, we apologize for any inconvenience this may have caused and thank you for your understanding and cooperation in this matter.
+\nOnce again, we apologize for any inconvenience this may have caused and thank you for your understanding and cooperation in this matter.
 
-\n\nThank you for considering DermaByte . We look forward to the possibility of working with you in the future.
+\nThank you for considering DermaByte . We look forward to the possibility of working with you in the future.
 
-\n\nSincerely,
-\nThe DermaByte application Team
+\nSincerely,
+The DermaByte application Team
  `;
 
       return sendEmail({
